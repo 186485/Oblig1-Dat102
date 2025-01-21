1de46798a91182cc5848dc3f10d4d76c2bb7d4ec
186485 <kasper.austboe@gmail.com>
2025-01-21 12:48:09 +0100

    Oppgave1.1
    PUSHPUSHPUSh

---
+++ Oblig1/src/no/hvl/data102/filmarkiv/impl/Film.java
@@ -0,0 +1,85 @@
+package no.hvl.data102.filmarkiv.impl;
+
+import java.util.Objects;
+
+public class Film {
+	
+	private int Filmnr;
+	private String Produsent;
+	private String Tittel;
+	private int Lansering;
+	private String s;
+	Sjanger sjanger = Sjanger.valueOf(s);
+	private String FilmSelskap;
+	
+
+	private Film(int filmnr, String produsent, String tittel, int lansering, String sjanger, String filmselskap) {
+		this.Filmnr = filmnr;
+		this.Produsent = produsent;
+		this.Tittel = tittel;
+		this.Lansering = lansering;
+		this.s = sjanger;
+		this.FilmSelskap = filmselskap;
+		
+	}
+public int getFilmnr() {
+	return Filmnr;
+}
+public void setFilmnr(int filmnr) {
+	Filmnr = filmnr;
+}
+public String getProdusent() {
+	return Produsent;
+}
+public void setProdusent(String produsent) {
+	Produsent = produsent;
+}
+public String getTittel() {
+	return Tittel;
+}
+public void setTittel(String tittel) {
+	Tittel = tittel;
+}
+public int getLansering() {
+	return Lansering;
+}
+public void setLansering(int lansering) {
+	Lansering = lansering;
+}
+public Sjanger getSjanger() {
+	return sjanger;
+}
+public void setSjanger(Sjanger sjanger) {
+	this.sjanger = sjanger;
+}
+public String getFilmSelskap() {
+	return FilmSelskap;
+}
+public void setFilmSelskap(String filmSelskap) {
+	FilmSelskap = filmSelskap;
+}
+
+
+
+@Override
+public int hashCode() {
+	return Objects.hash(Filmnr);
+}
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Film other = (Film) obj;
+	return Filmnr == other.Filmnr;
+}
+
+Film N1 = new Film();
+Film N2 = new Film(Filmnr, Produsent, Tittel, Lansering, s, FilmSelskap);
+
+
+
+}
